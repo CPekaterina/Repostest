8c16ac47e79130388303f1fe6b6d266a6199c050
CPekaterina <katya_ilina@hotmail.com>
2014-08-21 10:01:43 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    double h,x;
+    x = sqrt(2);
+    
+    double df2point = (f(x+h)-f(x))/h;
+    double df3point = (f(x+h)-f(x-h))/(2*h);
+    
+    
+    cin >> h;
+    
+    cout << "2-point derivative of 1/tan(x) at sqrt(2)" << df2point << endl;
+    cout << "3-point derivative of 1/tan(x) at sqrt(2)" << df3point << endl;
+    
     return 0;
 
+}
+double f (double x)
+{
+    return 1./tan(x);
 }
 
