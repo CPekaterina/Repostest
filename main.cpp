8d553f62a971ad5f01e09ace7b42b67563088dd4
CPekaterina <katya_ilina@hotmail.com>
2014-08-22 10:53:48 +0200

    main2

--- main.cpp
+++ main.cpp
@@ -1,27 +1,51 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 
 using namespace std;
 
+double f (double x);
+float f (float x);
+
 int main()
 {
-    double h,x;
+    double h,x, toterr, minerr2, minerr3, minstep2, minstep3;
     x = sqrt(2);
-    
-    double df2point = (f(x+h)-f(x))/h;
-    double df3point = (f(x+h)-f(x-h))/(2*h);
-    
-    
-    cin >> h;
-    
-    cout << "2-point derivative of 1/tan(x) at sqrt(2)" << df2point << endl;
-    cout << "3-point derivative of 1/tan(x) at sqrt(2)" << df3point << endl;
-    
+    double result = 1./3.;
+    minerr2=minerr3=result;
+
+
+    for (h=0.1 ;h > 1e-20; h/=10.){
+
+        double df2point = (f(x+h)-f(x))/h;
+        double df3point = (f(x+h)-f(x-h))/(2*h);
+
+
+        cout << "2-point derivative of atan(x) at sqrt(2) for steplength " << h << " is " << setprecision(10) << df2point << endl;
+        toterr = abs(result-df2point);
+        cout << "the total error is " << toterr << endl;
+
+        if(toterr < minerr2) {minerr2=toterr; minstep2=h;}
+
+        cout << "3-point derivative of atan(x) at sqrt(2) for steplength " << h << " is " << setprecision(10) << df3point << endl;
+        toterr =abs(result-df3point);
+        cout << "the total error is " << toterr << endl;
+
+        if(toterr < minerr3) {minerr3=toterr; minstep3=h;}    }
+
+    cout << "The smallest total error for the 2-point-derivative is " << minerr2 << endl;
+    cout << "The corresponding steplength is " << minstep2 << endl;
+    cout << "The smallest total error for the 3-point-derivative is " << minerr3 << endl;
+    cout << "The corresponding steplength is " << minstep3 << endl;
     return 0;
 
 }
 double f (double x)
 {
-    return 1./tan(x);
+    return atan(x);
+}
+float f (float x)
+{
+    return atan(x);
 }
 
